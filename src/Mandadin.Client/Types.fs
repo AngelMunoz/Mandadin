1dd4249c3379833a7edf3431327e44971d881a66
Angel D. Munoz <scythes.lol@gmail.com>
2020-09-18 20:25:50 -0600

    add icons

--- src/Mandadin.Client/Types.fs
+++ src/Mandadin.Client/Types.fs
@@ -1,7 +1,7 @@
 namespace Mandadin.Client
 
 open Bolero
-open System
+open Bolero.Html
 
 [<RequireQualifiedAccess>]
 type View =
@@ -21,11 +21,16 @@ type Note =
   {
     Id: string
     Content: string
-    Hide: bool
+    Rev: string
   }
 
 
-type TrackList = { Id: string; Name: string }
+type TrackList =
+  {
+    Id: string
+    Name: string
+    Rev: string
+  }
 
 type TrackListItem =
   {
@@ -33,4 +38,29 @@ type TrackListItem =
     Name: string
     IsDone: bool
     ListId: string
+    Rev: string
   }
+
+type Icon =
+  | Copy
+  | Share
+  | Trash
+  | Clipboard
+  | Save
+
+[<RequireQualifiedAccess>]
+module Icon =
+  type Copy = Template<"wwwroot/icons/copy.html">
+  type Share = Template<"wwwroot/icons/share.html">
+  type Trash = Template<"wwwroot/icons/trash.html">
+  type Clipboard = Template<"wwwroot/icons/clipboard.html">
+  type Save = Template<"wwwroot/icons/save.html">
+
+  let Get (icon: Icon) (color: Option<string>): Node =
+    let color = defaultArg color "currentColor"
+    match icon with
+    | Copy -> Copy().Fill(color).Elt()
+    | Share -> Share().Fill(color).Elt()
+    | Trash -> Trash().Fill(color).Elt()
+    | Clipboard -> Clipboard().Fill(color).Elt()
+    | Save -> Save().Fill(color).Elt()
