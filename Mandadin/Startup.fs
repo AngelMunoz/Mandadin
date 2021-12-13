a8dc9aaba6b869c1a10b83effcd0423065df68c6
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-13 14:51:22 -0600

    simplify Startup.fs

--- Mandadin/Startup.fs
+++ Mandadin/Startup.fs
@@ -1,39 +1,35 @@
-namespace Mandadin
-
+open System
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.JSInterop
+open Mandadin
 
+let builder =
+    WebAssemblyHostBuilder.CreateDefault(Environment.GetCommandLineArgs())
 
-module Program =
+builder.RootComponents.Add<Main.Mandadin>("#main")
 
-  [<EntryPoint>]
-  let Main args =
-    let builder =
-      WebAssemblyHostBuilder.CreateDefault(args)
+builder.Services.AddScoped<IClipboardService>
+    (fun services ->
+        let jsRuntime = services.GetService<IJSRuntime>()
+        Clipboard.GetService jsRuntime)
+|> ignore
 
-    builder.RootComponents.Add<Main.Mandadin>("#main")
+builder.Services.AddScoped<IShareService>
+    (fun services ->
+        let jsRuntime = services.GetService<IJSRuntime>()
+        Share.GetService jsRuntime)
+|> ignore
 
-    builder.Services.AddScoped<IClipboardService> (fun services ->
-      let jsRuntime = services.GetService<IJSRuntime>()
-      Clipboard.GetService jsRuntime)
-    |> ignore
+builder.Services.AddScoped<ITrackListItemService>
+    (fun services ->
+        let jsRuntime = services.GetService<IJSRuntime>()
+        let clipboard = services.GetService<IClipboardService>()
+        let share = services.GetService<IShareService>()
+        TrackListItem.GetService jsRuntime share clipboard)
+|> ignore
 
-    builder.Services.AddScoped<IShareService> (fun services ->
-      let jsRuntime = services.GetService<IJSRuntime>()
-      Share.GetService jsRuntime)
-    |> ignore
+builder.Build().RunAsync()
+|> Async.AwaitTask
+|> Async.Start
 
-    builder.Services.AddScoped<ITrackListItemService> (fun services ->
-      let jsRuntime = services.GetService<IJSRuntime>()
-      let clipboard = services.GetService<IClipboardService>()
-      let share = services.GetService<IShareService>()
-      TrackListItem.GetService jsRuntime share clipboard)
-    |> ignore
-
-    builder.Build().RunAsync()
-    |> Async.AwaitTask
-    |> Async.Start
-    |> ignore
-
-    0
