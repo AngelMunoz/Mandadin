df2bc2f1d37edbc73dd189dd84cdf7843d4e1dbe
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-13 23:20:50 -0600

    remove theming from main app

--- Mandadin/Main.fs
+++ Mandadin/Main.fs
@@ -12,19 +12,14 @@ module Main =
 
   type State =
     { View: View
-      Theme: Theme
       CanShare: bool
       HasOverlayControls: bool
       Title: string }
 
   type Msg =
     | SetView of View
-    | TryChangeTheme of Theme
-    | ChangeThemeSuccess of bool * Theme
     | GetHasOverlay
     | GetHasOverlaySuccess of bool
-    | GetTheme
-    | GetThemeSuccess of string
     | CanShare
     | CanShareSuccess of bool
     | Error of exn
@@ -49,61 +44,38 @@ module Main =
 
   let private init (_: 'arg) : State * Cmd<Msg> =
     { View = View.Notes
-      Theme = Theme.Dark
       CanShare = false
       HasOverlayControls = false
       Title = getGreeting () },
-    Cmd.batch [ Cmd.ofMsg GetTheme
-                Cmd.ofMsg CanShare
-                Cmd.ofMsg GetHasOverlay ]
+    Cmd.batch [
+      Cmd.ofMsg CanShare
+      Cmd.ofMsg GetHasOverlay
+    ]
 
-  let private update (msg: Msg) (state: State) (js: IJSRuntime) : State * Cmd<Msg> =
+  let private update
+    (msg: Msg)
+    (state: State)
+    (js: IJSRuntime)
+    : State * Cmd<Msg> =
     match msg with
     | SetView view ->
       { state with
           View = view
           Title = getGreeting () },
       Cmd.none
-    | TryChangeTheme theme ->
-      let jsThemeArg =
-        match theme with
-        | Theme.Dark -> "Dark"
-        | _ -> "Light"
-
+    | GetHasOverlay ->
       state,
       Cmd.OfJS.either
         js
-        "Mandadin.Theme.SwitchTheme"
-        [| jsThemeArg |]
-        (fun didChange -> ChangeThemeSuccess(didChange, theme))
+        "Mandadin.Theme.HasOverlayControls"
+        [||]
+        GetHasOverlaySuccess
         Error
-    | ChangeThemeSuccess (didChange, theme) ->
-      if didChange then
-        { state with Theme = theme }, Cmd.none
-      else
-        state, Cmd.ofMsg (Error(exn "Failed to change theme"))
-    | GetHasOverlay ->
-      state,
-      Cmd.OfJS.either js "Mandadin.Theme.HasOverlayControls" [||] GetHasOverlaySuccess Error
     | GetHasOverlaySuccess hasOverlay ->
-      { state with
-          HasOverlayControls = hasOverlay },
-      Cmd.none
-    | GetTheme -> state, Cmd.OfJS.either js "Mandadin.Theme.GetTheme" [||] GetThemeSuccess Error
-    | GetThemeSuccess theme ->
-      let theme =
-        match theme with
-        | "Light" -> Theme.Light
-        | _ -> Theme.Dark
-
-      let cmd =
-        if theme <> state.Theme then
-          Cmd.ofMsg (TryChangeTheme theme)
-        else
-          Cmd.none
-
-      { state with Theme = theme }, cmd
-    | CanShare -> state, Cmd.OfJS.either js "Mandadin.Share.CanShare" [||] CanShareSuccess Error
+      { state with HasOverlayControls = hasOverlay }, Cmd.none
+    | CanShare ->
+      state,
+      Cmd.OfJS.either js "Mandadin.Share.CanShare" [||] CanShareSuccess Error
     | CanShareSuccess canShare -> { state with CanShare = canShare }, Cmd.none
     | Error err ->
       eprintfn "Update Error: [%s]" err.Message
@@ -115,17 +87,15 @@ module Main =
   let private navigateToList (dispatch: Dispatch<Msg>) (route: string) =
     View.ListDetail route |> SetView |> dispatch
 
-  let private goBack (dispatch: Dispatch<Msg>) () = SetView View.Lists |> dispatch
+  let private goBack (dispatch: Dispatch<Msg>) () =
+    SetView View.Lists |> dispatch
 
   let private view (state: State) (dispatch: Dispatch<Msg>) : Node =
-    let getRoute (view: View) = router.HRef view
-
-    let onThemeChangeRequest (theme: Theme) = TryChangeTheme theme |> dispatch
 
     article [ attr.``class`` "mandadin" ] [
       if state.HasOverlayControls then
         TitleBar.View(Some state.Title)
-      Navbar.View state.Theme onThemeChangeRequest getRoute
+      Navbar.View()
       main [ attr.``class`` "paper container mandadin-main" ] [
         match state.View with
         | View.Import ->
@@ -133,7 +103,8 @@ module Main =
             [ "OnGoToListRequested"
               => Some(navigateToList dispatch) ]
             []
-        | View.Notes -> comp<Views.Notes.Page> [ "CanShare" => state.CanShare ] []
+        | View.Notes ->
+          comp<Views.Notes.Page> [ "CanShare" => state.CanShare ] []
         | View.Lists ->
           comp<Views.Lists.Page>
             [ "OnRouteRequested"
@@ -150,9 +121,7 @@ module Main =
         p [] [
           text "\u00A9 Tunaxor Apps 2020 - 2021"
         ]
-        p [] [
-          text "Mandadin4"
-        ]
+        p [] [ text "Mandadin4" ]
       ]
     ]
 
