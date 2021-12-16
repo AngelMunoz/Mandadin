db9b4f8430ea5a2c3eeee9bf2c9d372b491f7179
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-16 12:42:05 -0600

    run prettier on it

--- Mandadin/wwwroot/js/types.d.ts
+++ Mandadin/wwwroot/js/types.d.ts
@@ -1,29 +1,26 @@
 
 type Theme =
     | 'Light'
-    | 'Dark'
-// Enable Custom Theme Later
-// | ['Custom', string[]]
+    | 'Dark';
 
 type Note = {
-    id: string
-    content: string
-    rev: string
-}
+    id: string;
+    content: string;
+    rev: string;
+};
 
 type List = {
-    id: string
-    rev: string
-}
+    id: string;
+    rev: string;
+};
 
 type ListItem = {
-    id: string
-    isDone: boolean
-    listId: string
-    name: string
-    rev: string
-}
-
+    id: string;
+    isDone: boolean;
+    listId: string;
+    name: string;
+    rev: string;
+};
 declare interface Window {
-    Mandadin: { Theme, Share, Clipboard, Database }
+    Mandadin: { Theme, Share, Clipboard, Database; };
 }
